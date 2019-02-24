897545c35cab37170ce2bc850c4a864e95ca2f00
CesarSG <sevillacesar84@gmail.com>
2019-02-23 20:21:09 -0600

    Actualizacion web.php

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::get('/causas', 'PaginasController@causes')->name('causes');
 Route::get('/galeria', 'PaginasController@gallery')->name('gallery');
 Route::get('/noticias', 'PaginasController@news')->name('news');
 Route::get('/contacto', 'PaginasController@contact')->name('contact');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
