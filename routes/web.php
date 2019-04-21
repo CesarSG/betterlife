ac0a63edd88e9e6dbb3db2cbcd43d5ddcb145612
kira187 Desktop <missael133@gmail.com>
2019-04-20 22:16:04 -0500

    Agregado de columna rol a usuario
    -columna rol a users
    -uso de prefix en rutas del dasboard

--- routes/web.php
+++ routes/web.php
@@ -10,27 +10,18 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/hola', function () {
-    return view('welcome');
-});
-
+Auth::routes(['verify'=>true]);
 Route::get('/', 'PaginasController@index')->name('index');
 Route::get('/nosotros', 'PaginasController@about')->name('about');
 Route::get('/causas', 'PaginasController@causes')->name('causes');
 Route::get('/galeria', 'PaginasController@gallery')->name('gallery');
 Route::get('/noticias', 'PaginasController@news')->name('news');
 Route::get('/contacto', 'PaginasController@contact')->name('contact');
-Route::get('/adminsss', 'PaginasController@admin')->name('admin');
-
-Route::get('/admin', 'PaginasController@dashboard')->name('dashboard');
-Route::get('/admin/usuario', 'PaginasController@user')->name('user');
-Route::get('/admin/tablas', 'PaginasController@tables')->name('tables');
-Route::get('/admin/team', 'PaginasController@team')->name('team');
-Route::get('/admin/notificacion', 'PaginasController@notification')->name('notification');
-
-Route::get('/entrar', 'PaginasController@entrar')->name('entrar');
-
-Auth::routes(['verify'=>true]);
-
 Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['prefix'=>'admin'], function(){
+  Route::get('', 'PaginasController@dashboard')->name('dashboard');
+  Route::get('usuario', 'PaginasController@user')->name('user');
+  Route::get('tablas', 'PaginasController@tables')->name('tables');
+  Route::get('team', 'PaginasController@team')->name('team');
+  Route::get('notificacion', 'PaginasController@notification')->name('notification');
+});
